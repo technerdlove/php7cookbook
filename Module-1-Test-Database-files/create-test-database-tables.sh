496c849e6703783bd16ce26610c3041b25b67622
technerdlove <technerdlove@users.noreply.github.com>
2017-10-15 16:45:22 -0700

    Update create-test-database-tables.sh

--- Module-1-Test-Database-files/create-test-database-tables.sh
+++ Module-1-Test-Database-files/create-test-database-tables.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-echo "Installing git..."
-yum -y install git
-
-
 # mysql -u root -pPASSWORD -D php7cookbook -e <<EOF
 # SQL_QUERY 1
 # SQL_QUERY 2
 # SQL_QUERY N
 # EOF
 
-
-mysql -u root -pPASSWORD -D php7cookbook -e <(curl -s https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-9-of-15-values-for-test-database-from-Module-1.sql?token=AOn44_hlgdvpSQV5D28VDx8I8N8u8lFPks5Z7SjTwA%3D%3D)
+echo "mysql -u root -pPASSWORD -D php7cookbook <(curl -s https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/{PART-1-of-15-values-for-test-database-from-Module-1.sql},{PART-2-of-15-values-for-test-database-from-Module-1.sql},{PART-3-of-15-values-for-test-database-from-Module-1.sql},{PART-4-of-15-values-for-test-database-from-Module-1.sql},{PART-5-of-15-values-for-test-database-from-Module-1.sql},{PART-6-of-15-values-for-test-database-from-Module-1.sql},{PART-7-of-15-values-for-test-database-from-Module-1.sql},{PART-8-of-15-values-for-test-database-from-Module-1.sql},{PART-9-of-15-values-for-test-database-from-Module-1.sql},{PART-10-of-15-values-for-test-database-from-Module-1.sql},{PART-11-of-15-values-for-test-database-from-Module-1.sql},{PART-12-of-15-values-for-test-database-from-Module-1.sql},{PART-13-of-15-values-for-test-database-from-Module-1.sql},{PART-14-of-15-values-for-test-database-from-Module-1.sql},{PART-15-of-15-values-for-test-database-from-Module-1.sql})"
 
