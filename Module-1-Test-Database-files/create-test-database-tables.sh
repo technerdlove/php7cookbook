b2c878daacc91bafe37ebc3234fa3225a4f6233f
technerdlove <technerdlove@users.noreply.github.com>
2017-10-15 16:26:34 -0700

    Update create-test-database-tables.sh

--- Module-1-Test-Database-files/create-test-database-tables.sh
+++ Module-1-Test-Database-files/create-test-database-tables.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-mysql -u root -pPASSWORD -D php7cookbook -e <<EOF
-SQL_QUERY 1
-SQL_QUERY 2
-SQL_QUERY N
-EOF
+echo "Installing git..."
+yum -y install git
+
+
+# mysql -u root -pPASSWORD -D php7cookbook -e <<EOF
+# SQL_QUERY 1
+# SQL_QUERY 2
+# SQL_QUERY N
+# EOF
+
+
+mysql -u root -pPASSWORD -D php7cookbook -e <(curl -s https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-9-of-15-values-for-test-database-from-Module-1.sql?token=AOn44_hlgdvpSQV5D28VDx8I8N8u8lFPks5Z7SjTwA%3D%3D)
+
