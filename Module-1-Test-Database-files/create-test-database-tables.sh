1fd74ea56c417011c5b165b9712c9a5c84ef218f
technerdlove <technerdlove@users.noreply.github.com>
2017-10-16 00:36:53 -0700

    Update create-test-database-tables.sh

--- Module-1-Test-Database-files/create-test-database-tables.sh
+++ Module-1-Test-Database-files/create-test-database-tables.sh
@@ -1,4 +1,4 @@
-# #!/bin/bash
+#!/bin/bash
 
 # mysql -u root -pPASSWORD -D php7cookbook -e <<EOF
 # SQL_QUERY 1
@@ -12,6 +12,9 @@
 
 # echo "Installing SQL statements via curl..."
 # <(curl -s https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-1-of-15-values-for-test-database-from-Module-1},{PART-2-of-15-values-for-test-database-from-Module-1},{PART-3-of-15-values-for-test-database-from-Module-1},{PART-4-of-15-values-for-test-database-from-Module-1},{PART-5-of-15-values-for-test-database-from-Module-1},{PART-6-of-15-values-for-test-database-from-Module-1},{PART-7-of-15-values-for-test-database-from-Module-1},{PART-8-of-15-values-for-test-database-from-Module-1},{PART-9-of-15-values-for-test-database-from-Module-1},{PART-10-of-15-values-for-test-database-from-Module-1},{PART-11-of-15-values-for-test-database-from-Module-1},{PART-12-of-15-values-for-test-database-from-Module-1},{PART-13-of-15-values-for-test-database-from-Module-1},{PART-14-of-15-values-for-test-database-from-Module-1},{PART-15-of-15-values-for-test-database-from-Module-1}.sql)
+
+
+echo "Create sql files in root tmp folder via curl"
 
 curl -o /tmp/PART-1-of-15-values-for-test-database.sql https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-1-of-15-values-for-test-database-from-Module-1.sql
 curl -o /tmp/PART-2-of-15-values-for-test-database.sql https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-2-of-15-values-for-test-database-from-Module-1.sql
@@ -28,3 +31,42 @@ curl -o /tmp/PART-12-of-15-values-for-test-database.sql https://raw.githubuserco
 curl -o /tmp/PART-13-of-15-values-for-test-database.sql https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-13-of-15-values-for-test-database-from-Module-1.sql
 curl -o /tmp/PART-14-of-15-values-for-test-database.sql https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-14-of-15-values-for-test-database-from-Module-1.sql
 curl -o /tmp/PART-15-of-15-values-for-test-database.sql https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-15-of-15-values-for-test-database-from-Module-1.sql
+
+# Connect to local mysql php7cookbook database with root as user, prompt for password
+mysql -u root -p php7cookbook
+
+# Execute sql files in /tmp folder
+source /tmp/PART-1-of-15-values-for-test-database.sql
+source /tmp/PART-2-of-15-values-for-test-database.sql
+source /tmp/PART-3-of-15-values-for-test-database.sql
+source /tmp/PART-4-of-15-values-for-test-database.sql
+source /tmp/PART-5-of-15-values-for-test-database.sql
+source /tmp/PART-6-of-15-values-for-test-database.sql
+source /tmp/PART-7-of-15-values-for-test-database.sql
+source /tmp/PART-8-of-15-values-for-test-database.sql
+source /tmp/PART-9-of-15-values-for-test-database.sql
+source /tmp/PART-10-of-15-values-for-test-database.sql
+source /tmp/PART-11-of-15-values-for-test-database.sql
+source /tmp/PART-12-of-15-values-for-test-database.sql
+source /tmp/PART-13-of-15-values-for-test-database.sql
+source /tmp/PART-14-of-15-values-for-test-database.sql
+source /tmp/PART-15-of-15-values-for-test-database.sql
+
+
+
+########
+source /tmp/PART-1-of-15-values-for-test-database
+source /tmp/PART-2-of-15-values-for-test-database
+source /tmp/PART-3-of-15-values-for-test-database
+source /tmp/PART-4-of-15-values-for-test-database
+source /tmp/PART-5-of-15-values-for-test-database
+source /tmp/PART-6-of-15-values-for-test-database
+source /tmp/PART-7-of-15-values-for-test-database
+source /tmp/PART-8-of-15-values-for-test-database
+source /tmp/PART-9-of-15-values-for-test-database
+source /tmp/PART-10-of-15-values-for-test-database
+source /tmp/PART-11-of-15-values-for-test-database
+source /tmp/PART-12-of-15-values-for-test-database
+source /tmp/PART-13-of-15-values-for-test-database
+source /tmp/PART-14-of-15-values-for-test-database
+source /tmp/PART-15-of-15-values-for-test-database
