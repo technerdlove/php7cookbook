5eb680eab770ba2ef3cf9152dbf26689cc404d11
technerdlove <technerdlove@users.noreply.github.com>
2017-10-18 12:27:16 -0700

    Update create-test-database-tables.sh

--- Module-1-Test-Database-files/create-test-database-tables.sh
+++ Module-1-Test-Database-files/create-test-database-tables.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# echo "Installing SQL statements via curl..."
-# mysql -u root -pPASSWORD -D php7cookbook <(curl -s https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/{PART-1-of-15-values-for-test-database-from-Module-1.sql},{PART-2-of-15-values-for-test-database-from-Module-1.sql},{PART-3-of-15-values-for-test-database-from-Module-1.sql},{PART-4-of-15-values-for-test-database-from-Module-1.sql},{PART-5-of-15-values-for-test-database-from-Module-1.sql},{PART-6-of-15-values-for-test-database-from-Module-1.sql},{PART-7-of-15-values-for-test-database-from-Module-1.sql},{PART-8-of-15-values-for-test-database-from-Module-1.sql},{PART-9-of-15-values-for-test-database-from-Module-1.sql},{PART-10-of-15-values-for-test-database-from-Module-1.sql},{PART-11-of-15-values-for-test-database-from-Module-1.sql},{PART-12-of-15-values-for-test-database-from-Module-1.sql},{PART-13-of-15-values-for-test-database-from-Module-1.sql},{PART-14-of-15-values-for-test-database-from-Module-1.sql},{PART-15-of-15-values-for-test-database-from-Module-1.sql})
-
-
-# echo "Installing SQL statements via curl..."
-# <(curl -s https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-1-of-15-values-for-test-database-from-Module-1},{PART-2-of-15-values-for-test-database-from-Module-1},{PART-3-of-15-values-for-test-database-from-Module-1},{PART-4-of-15-values-for-test-database-from-Module-1},{PART-5-of-15-values-for-test-database-from-Module-1},{PART-6-of-15-values-for-test-database-from-Module-1},{PART-7-of-15-values-for-test-database-from-Module-1},{PART-8-of-15-values-for-test-database-from-Module-1},{PART-9-of-15-values-for-test-database-from-Module-1},{PART-10-of-15-values-for-test-database-from-Module-1},{PART-11-of-15-values-for-test-database-from-Module-1},{PART-12-of-15-values-for-test-database-from-Module-1},{PART-13-of-15-values-for-test-database-from-Module-1},{PART-14-of-15-values-for-test-database-from-Module-1},{PART-15-of-15-values-for-test-database-from-Module-1}.sql)
-
 # Use curl to pull files from GitHub and store in a new file root tmp folder
 # Don't include .sql extension in the name of the new file, because it just gets chopped off during the curl process.
+# This script could probably be optimized for performance, but that's a to-do.  This shows how it gets done pretty clearly, while also doing it.
 echo "Create sql files in root tmp folder via curl"
 
 curl -o /tmp/PART-1-of-15-values-for-test-database https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-1-of-15-values-for-test-database-from-Module-1.sql
@@ -52,39 +46,3 @@ source /tmp/PART-14-of-15-values-for-test-database
 source /tmp/PART-15-of-15-values-for-test-database
 exit
 
-
-# Try this as alternative
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-1-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-2-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-3-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-4-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-5-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-6-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-7-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-8-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-9-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-10-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-11-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-12-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-13-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-14-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-15-of-15-values-for-test-database-from-Module-1.sql)
-exit
-
-# Try this as alternative
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-1-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-2-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-3-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-4-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-5-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-6-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-7-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-8-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-9-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-10-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-11-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-12-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-13-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-14-of-15-values-for-test-database-from-Module-1.sql)
-source <(curl -s  https://raw.githubusercontent.com/technerdlove/php7cookbook/master/Module-1-Test-Database-files/PART-15-of-15-values-for-test-database-from-Module-1.sql)
-exit
